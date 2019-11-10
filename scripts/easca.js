56c287b56218d43dffff79088053b36bff5d4ac5
ribo <roibeard.mac.unfraidh@gmail.com>
2019-11-10 13:17:33 +0100

    Delete output on arrow key press.

--- scripts/easca.js
+++ scripts/easca.js
@@ -3,6 +3,10 @@ let keyPressed;
 $('.btn-secondary').click(function(){
 	keyPressed = (this.innerHTML);
 	$('#output').append(keyPressed)
+	var id = this.id
+	if (id ==='return'){
+		$('#output').empty();
+	}
 });
 
 
