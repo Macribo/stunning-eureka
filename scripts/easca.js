78784be8e21c21a166e71521fac8f9a9f35ebe24
ribo <roibeard.mac.unfraidh@gmail.com>
2019-11-11 13:00:20 +0100

    improve key spacing.

--- scripts/easca.js
+++ scripts/easca.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-let keyPressed;
-$('.btn-secondary').click(function(){
-	keyPressed = (this.innerHTML);
-	$('#output').append(keyPressed)
-	var id = this.id
-	if (id ==='return'){
-		$('#output').empty();
-	}
-});
+	let keyPressed;
+		$('.btn-secondary').click(function(){
+			keyPressed = (this.innerHTML);
+			$('#output').append(keyPressed)
+			var id = this.id
+			if (id ==='return'){
+				$('#output').empty();
+			}
+		});
+
 
 
 
