391431ef96a118e57919bc72f0ad9e3c51428609
ribo <roibeard.mac.unfraidh@gmail.com>
2019-11-12 13:07:23 +0100

    Reverse right hand key order.

--- scripts/easca.js
+++ scripts/easca.js
@@ -6,7 +6,12 @@ $(document).ready(function () {
 			var id = this.id
 			if (id ==='return'){
 				$('#output').empty();
+			
 			}
+			// if (id === 'undo'){
+			// 	let temp = document.getElementById('#output');
+			// 	alert(temp)
+			// }
 		});
 
 
